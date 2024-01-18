a04991a826f453668fadf7333c460b8ffaa2584c
Oladele Joshua <joshuaxp9@gmail.com>
2024-01-18 03:31:42 +0100

    Add files via upload

--- client/src/pages/pay/Pay.jsx
+++ client/src/pages/pay/Pay.jsx
@@ -1,79 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { loadStripe } from '@stripe/stripe-js';
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout
-} from '@stripe/react-stripe-js';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useParams } from "react-router-dom";
+import { Axios } from "../../config";
+import requests from "../../libs/request";
+import CheckoutForm from "../../components/PayContents/CheckoutForm/CheckoutForm";
 
-const stripePromise = loadStripe("pk_test_51OPujsGtiWlekgp2NSbDVq0Ne20b0KAHHAM3QWuz42Lzyp6g3Sp0qJfmUrQLHu0kqg5gdIQ0MFEMqDnZGS9MEkSz00QfSqxWlH");
+const stripePromise = loadStripe(
+  "pk_test_51OPujsGtiWlekgp2NSbDVq0Ne20b0KAHHAM3QWuz42Lzyp6g3Sp0qJfmUrQLHu0kqg5gdIQ0MFEMqDnZGS9MEkSz00QfSqxWlH"
+);
 
-const CheckoutForm = () => {
-  const [clientSecret, setClientSecret] = useState('');
-
+const Pay = () => {
+  const { id } = useParams();
+  const [clientSecret, setClientSecret] = useState("");
   useEffect(() => {
-    fetch("/create-checkout-session", { method: "POST" })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
+    const makeRequest = async () => {
+      try {
+        const res = await Axios.post(
+          `${requests.orders}/create-payment-intent/${id}`
+        );
+        setClientSecret(res.data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
   }, []);
 
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
-    <div id="checkout">
+    <div className="py-40 pb-10">
       {clientSecret && (
-        <EmbeddedCheckoutProvider stripe={stripePromise} options={{ clientSecret }}>
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
       )}
     </div>
   );
-}
-
-const Return = () => {
-  const [status, setStatus] = useState(null);
-  const [customerEmail, setCustomerEmail] = useState('');
-
-  useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const sessionId = urlParams.get('session_id');
-
-    fetch(`/session-status?session_id=${sessionId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setStatus(data.status);
-        setCustomerEmail(data.customer_email);
-      });
-  }, []);
-
-  if (status === 'open') {
-    return <Navigate to="/checkout" />;
-  }
-
-  if (status === 'complete') {
-    return (
-      <section id="success">
-        <p>
-          We appreciate your business! A confirmation email will be sent to {customerEmail}.
-          If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
-        </p>
-      </section>
-    );
-  }
-
-  return null;
-}
-
-const Pay = () => {
-  return (
-    <div className="Pay">
-      <Router>
-        <Routes>
-          <Route path="/checkout" element={<CheckoutForm />} />
-          <Route path="/return" element={<Return />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+};
 
 export default Pay;
